09067d3c13d90198576ba10d174032278b0ac9ec
dcl10 <lea.daniel@hotmail.co.uk>
2017-10-24 16:54:03 +0100

    Unsuccessful playing around with script. May soon be able to tell which patients fall into the clusters

--- cluster_outcomes.py
+++ cluster_outcomes.py
@@ -1,14 +1,36 @@
-import collections
-import pandas
-import matplotlib.pyplot as plt
+import collections, pandas, matplotlib.pyplot as plt, nltk
 
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, names, words
 from nltk.tokenize import word_tokenize
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-df = pandas.read_csv("../../Data/merged_on_IPF.csv", na_filter='')
+df = pandas.read_csv("../../Data/merged_on_Emphysema.csv", na_filter='')
 texts = list(df['Outcome'])
+
+# Refinement by removal of junk words
+sw = list(stopwords.words('english'))
+sw.extend(["?", "<", ">", ",", ".", '+', '-', '(', ')', '%'])
+name_list = list(names.words('male.txt'))
+name_list.extend(list(names.words('female.txt'))+['goodman', 'mr', 'mrs', 'dr', 'miss'])
+# Names are capitalised, so not recognised when trying to remove them. Thus make them all lowercase
+for i in range(len(name_list)):
+     name_list[i] = str(name_list[i]).lower()
+# To remove over simple English words
+basic_words = list(words.words('en-basic'))
+
+# Extract all words that are not stop words or basic English
+def remove_junk(text):
+    all_words = []
+    for j in word_tokenize(text.lower()):
+        if j not in name_list and j not in sw and j not in basic_words:
+            all_words.append(j)
+    # convert to sentence
+    doc = "".join(word + " " for word in all_words)
+    return doc
+
+# Remove junk words from the documents
+texts = list(remove_junk(str(i)) for i in texts)
 
 # robbed from: "http://nlpforhackers.io/recipe-text-clustering/"
 def cluster_texts(texts, clusters=3):
@@ -22,8 +44,11 @@ def cluster_texts(texts, clusters=3):
     tfidf_model = vectorizer.fit_transform(texts)
     km_model = KMeans(n_clusters=clusters)
     km_model.fit(tfidf_model)
+
+    # the following two lines are my adaptation
+    # trans is the cluster-distance space for the dataset to be plotted
+    # labels are the clusters each document belongs to
     trans = km_model.transform(tfidf_model)
-    centres = km_model.cluster_centers_
     labels = km_model.labels_
 
     clustering = collections.defaultdict(list)
@@ -31,12 +56,24 @@ def cluster_texts(texts, clusters=3):
     for idx, label in enumerate(km_model.labels_):
         clustering[label].append(idx)
 
-    return clustering, centres, trans, labels
+    # modified to return centres, trans and labels
+    return clustering, trans, labels
 
-clusters, centres, coordinates, lables= cluster_texts(texts, clusters=2)
+clusters, coordinates, labels = cluster_texts(texts, clusters=2)
 colours = ['red', 'blue', 'green', 'yellow', 'orange', 'pink', 'purple']
 
-for i in range(len(lables)):
-    plt.scatter(coordinates[i][0], coordinates[i][1], color=colours[lables[i]])
+for i in range(len(labels)):
+    plt.scatter(coordinates[i][0], coordinates[i][1], color=colours[labels[i]])
 
+plt.title("Plot of cluster-distance matrix from k means clustering")
 plt.show()
+
+cluster_0_docs = [texts[i] for i in clusters[0]]
+cluster_1_docs = [texts[i] for i in clusters[1]]
+print(cluster_0_docs)
+print(cluster_1_docs)
+words_0 = []
+for i in cluster_0_docs:
+    words_0.extend(word_tokenize(i))
+
+print(words_0)
